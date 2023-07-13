5502cbbbcbd7686d0bcc7f3452dd82ab4deee886
toufikbakhtaoui <58857863+toufikbakhtaoui@users.noreply.github.com>
2023-07-14 01:50:18 +0200

    Initial commit

---
+++ src/main/java/com/solyador/tickers/TickersApplication.java
@@ -0,0 +1,11 @@
+package com.solyador.tickers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickersApplication {
+    public static void main(String[] args) {
+       SpringApplication.run(TickersApplication.class, args);
+    }
+}
